216bd9997343e4f74a5b25e3cb43f77c8f6ee83d
Sviatoslav000 <svyatyablonsky@gmail.com>
2026-04-04 13:39:02 +0000

    git actions

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,5 @@ terraform {
   backend "gcs" {
     bucket = "terraform-state-lab3-devops2"
     prefix = "terraform/state"
-    credentials = "/home/kolinssviatoslav/yablonskyi-lab3/terraformkey.json"
   }
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,6 +2,5 @@ provider "google" {
   project = var.project_id
   region = var.region
   zone = var.zone
-  credentials = file("/home/kolinssviatoslav/yablonskyi-lab3/terraformkey.json")
 }
 
