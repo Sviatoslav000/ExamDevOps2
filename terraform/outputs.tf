fcafe46a981ebddab45cd6c9660136c454947a87
Sviatoslav000 <svyatyablonsky@gmail.com>
2026-04-04 21:44:38 +0000

    Commit_1

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -13,19 +13,3 @@ output "vpc_id" {
   value = google_compute_network.vpc.id
 
 }
-
-output "ansible_private_key" {
-
-  description = "Path to private SSH key"
-
-  value = local_file.private_key.filename
-
-}
-
-output "ansible_inventory" {
-
-  description = "Generated Ansible inventory"
-
-  value = "../ansible/inventory.ini"
-
-}
