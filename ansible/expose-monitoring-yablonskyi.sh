93f2948579def84a94c1e2a4a2fc57eb96002d5e
Sviatoslav000 <svyatyablonsky@gmail.com>
2026-04-05 20:47:36 +0000

    Task2 commit1

---
+++ ansible/expose-monitoring-yablonskyi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Starting port forwarding for Monitoring Stack..."
+
+# Namespace
+NAMESPACE="monitoring-yablonskyi"  # <- замініть на своє прізвище / namespace
+
+# 1️⃣rafana -> Port 8000
+nohup kubectl port-forward svc/monitoring-stack-grafana 80:80 -n monitoring-yablonskyi --address 0.0.0.0 > /dev/null 2>&1 &
+echo " -> Grafana exposed on port 80"
+
+# 2️⃣rometheus -> Port 908001
+nohup kubectl port-forward svc/monitoring-stack-kube-prom-prometheus 8000:9090 -n monitoring-yablonskyi --address 0.0.0.0 > /dev/null 2>&1 &
+echo " -> Prometheus exposed on port 8000"
+
+# 3️⃣lertmanager -> Port 908002
+nohup kubectl port-forward svc/monitoring-stack-kube-prom-alertmanager 8001:9093 -n monitoring-yablonskyi --address 0.0.0.0 > /dev/null 2>&1 &
+echo " -> Alertmanager exposed on port 8001"
+
+echo "Port forwarding started. Access your services via http://<VM-IP>:80, 8000, 8001"
